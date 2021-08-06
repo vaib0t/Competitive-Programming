6cdd1572e4b227d947b78ecf02a2bb2a49aa1809
Vaibhav Goyal <goyal.vaibhav1610@gmail.com>
2021-08-06 14:00:00 +0100

    Max Absolute diff

---
+++ Interview Bit/Maximum Absolute Difference.cpp
@@ -0,0 +1,18 @@
+int Solution::maxArr(vector<int> &A) {
+    int n=A.size();
+    int max1 = INT_MIN, min1 = INT_MAX;
+	int max2 = INT_MIN, min2 = INT_MAX;
+
+	for (int i = 0; i < n; i++) {
+
+		// Updating max and min variables
+		// as described in algorithm.
+		max1 = max(max1, A[i] + i);
+		min1 = min(min1, A[i] + i);
+		max2 = max(max2, A[i] - i);
+		min2 = min(min2, A[i] - i);
+	}
+
+	// Calculating maximum absolute difference.
+	return max(max1 - min1, max2 - min2);
+}
